24de184e1ec27aabda4d4f83167f60e57f9dc07b
Fauzan Hibatullah Ashari <fauzansaja10@gmail.com>
2021-07-17 14:15:27 +0700

    add more dummy data

--- db/database.js
+++ db/database.js
@@ -6,7 +6,23 @@ const products = [
         sell_price: 345000000,
         stock: 17,
         img: "index.jpg"
-    }
+    },
+    {
+        id: "af7k9",
+        name: "Mobil Type B",
+        purchase_price: 304000000,
+        sell_price: 345000000,
+        stock: 22,
+        img: "index.jpg"
+    },
+    {
+        id: "lk93v",
+        name: "Mobil Type C",
+        purchase_price: 345000000,
+        sell_price: 405000000,
+        stock: 9,
+        img: "index.jpg"
+    },
 ];
 
 module.exports = products;
